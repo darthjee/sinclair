b79025cca11e8827373364f8caad66291f8e32d6
Fernando Favini <darthjee@gmail.com>
2018-04-25 16:22:46 -0300

    Add spec for non extending build

--- spec/lib/concern_builder_spec.rb
+++ spec/lib/concern_builder_spec.rb
@@ -2,16 +2,17 @@ require 'spec_helper'
 
 describe ConcernBuilder do
   let(:options) { {} }
-  let(:instance) { ConcernBuilder::Dummy.new }
-  subject { builder_class.new(ConcernBuilder::Dummy, options) }
-
-  before do
-    subject.init
-    subject.build
-  end
+  let(:instance) { dummy_class.new }
+  subject { builder_class.new(dummy_class, options) }
 
   context 'when extending the class' do
-    let(:builder_class) { described_class::Dummy::Builder }
+    let(:builder_class) { dummy_class::Builder }
+    let(:dummy_class) { described_class::Dummy }
+
+    before do
+      subject.init
+      subject.build
+    end
 
     context 'when describing a method with block' do
       it 'creates a method with the block' do
@@ -31,6 +32,35 @@ describe ConcernBuilder do
       it 'parses the options' do
         expect(instance.defined).to eq(2)
         expect(instance.defined).to eq(4)
+      end
+    end
+  end
+
+  context 'when using the builder without extending' do
+    let(:builder_class) { described_class }
+    let(:dummy_class) { Class.new }
+
+    context 'when declaring a method with a block' do
+      before do
+        subject.add_method(:blocked) { 1 }
+        subject.add_method(:blocked) { 2 }
+        subject.build
+      end
+
+      it 'respect the order of method addtion' do
+        expect(instance.blocked).to eq(2)
+      end
+    end
+
+    context 'when declaring a method string' do
+      before do
+        subject.add_method(:string, '1')
+        subject.add_method(:string, '2')
+        subject.build
+      end
+
+      it 'respect the order of method addtion' do
+        expect(instance.string).to eq(2)
       end
     end
   end
