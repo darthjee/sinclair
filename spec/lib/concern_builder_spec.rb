d476fb86b6dc1bba53024e4c19819a4ab8ed1b9c
darthjee <darthjee@gmail.com>
2018-05-18 20:03:33 +0200

    Add more specs for eval_and_add_method

--- spec/lib/concern_builder_spec.rb
+++ spec/lib/concern_builder_spec.rb
@@ -92,23 +92,52 @@ describe ConcernBuilder do
   end
 
   describe '#eval_and_add_method' do
-    before do
-      subject.add_method(:value, "@value ||= 0")
-      subject.eval_and_add_method(:defined) { "@value = value + #{ options_object.increment || 1 }" }
-      subject.build
-    end
+    context 'when defining the method once' do
+      before do
+        subject.add_method(:value, "@value ||= 0")
+        subject.eval_and_add_method(:defined) { "@value = value + #{ options_object.increment || 1 }" }
+        subject.build
+      end
 
-    it 'creates a method using the string definition' do
-      expect(instance.defined).to eq(1)
-      expect(instance.defined).to eq(2)
-    end
-
-    context 'when passing options' do
-      let(:options) { { increment: 2 } }
-
-      it 'parses the options' do
+      it 'creates a method using the string definition' do
+        expect(instance.defined).to eq(1)
         expect(instance.defined).to eq(2)
-        expect(instance.defined).to eq(4)
+      end
+
+      context 'when passing options' do
+        let(:options) { { increment: 2 } }
+
+        it 'parses the options' do
+          expect(instance.defined).to eq(2)
+          expect(instance.defined).to eq(4)
+        end
+      end
+    end
+
+    context 'when redefining a method already added' do
+      before do
+        subject.add_method(:value,   "@value ||= 0")
+        subject.add_method(:defined, "100")
+        subject.eval_and_add_method(:defined) { "@value = value + #{ options_object.increment || 1 }" }
+        subject.build
+      end
+
+      it 'redefines it' do
+        expect(instance.defined).to eq(1)
+        expect(instance.defined).to eq(2)
+      end
+    end
+
+    context 'when readding it' do
+      before do
+        subject.add_method(:value,   "@value ||= 0")
+        subject.eval_and_add_method(:defined) { "@value = value + #{ options_object.increment || 1 }" }
+        subject.add_method(:defined, "100")
+        subject.build
+      end
+
+      it 'redefines it' do
+        expect(instance.defined).to eq(100)
       end
     end
   end
