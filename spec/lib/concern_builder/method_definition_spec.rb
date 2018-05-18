bc36ef105d81dec9f328f0962c08141e286a90ac
darthjee <darthjee@gmail.com>
2018-05-18 17:53:54 +0200

    Add method definition spec

---
+++ spec/lib/concern_builder/method_definition_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+describe ConcernBuilder::MethodDefinition do
+  let(:clazz) { Class.new }
+  let(:instance) { clazz.new }
+
+  describe '#build' do
+    let(:method_name) { :the_method }
+
+    context 'when method was defined with an string' do
+      let(:code) { '"Self ==> " + self.to_s' }
+
+      subject { described_class.new(clazz, method_name, code) }
+
+      before do
+        subject.build
+      end
+
+      it 'adds the method to the clazz instance' do
+        expect(instance).to respond_to(method_name)
+      end
+
+      it 'evaluates return of the method within the instance context' do
+        expect(instance.the_method).to eq("Self ==> #{instance}")
+      end
+    end
+
+    context 'when method was defined with a block' do
+      subject do
+        described_class.new(clazz, method_name) do
+          "Self ==> " + self.to_s
+         end
+      end
+
+      before do
+        subject.build
+      end
+
+      it 'adds the method to the clazz instance' do
+        expect(instance).to respond_to(method_name)
+      end
+
+      it 'evaluates return of the method within the instance context' do
+        expect(instance.the_method).to eq("Self ==> #{instance}")
+      end
+    end
+  end
+end
