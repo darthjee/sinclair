bdcdf8aea57654b063e6cc44ec26f3c75ff8c34f
darthjee <darthjee@gmail.com>
2018-07-02 19:26:19 +0200

    Add more matcher integration specs

--- spec/integration/matcher_spec.rb
+++ spec/integration/matcher_spec.rb
@@ -27,5 +27,30 @@ describe 'matchers' do
         )
       end
     end
+
+    context "when negating" do
+      let(:expectation) do
+        expect { block.call }.not_to add_method(method).to(instance)
+      end
+
+      context 'when method is not added' do
+        let(:block) { Proc.new {} }
+
+        it 'returns a succes' do
+          expect { expectation }.not_to raise_error
+        end
+      end
+
+      context 'when method is added' do
+        let(:block) { Proc.new { klass.send(:define_method, method) {} } }
+
+        it 'raises expectation error' do
+          expect { expectation }.not_to raise_error(
+            RSpec::Expectations::ExpectationNotMetError,
+            "expected 'the_method' to be added to #{klass} but it didn't"
+          )
+        end
+      end
+    end
   end
 end
