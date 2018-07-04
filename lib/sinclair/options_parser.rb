ffe8f78f99acf17085119d49580704dbd0f76297
darthjee <darthjee@gmail.com>
2018-07-05 00:57:34 +0200

    Add documentation for OptionParser

--- lib/sinclair/options_parser.rb
+++ lib/sinclair/options_parser.rb
@@ -1,9 +1,36 @@
-module Sinclair::OptionsParser
-  extend ActiveSupport::Concern
+class Sinclair
+  # Concern for easily adding options
+  #
+  # @example
+  #
+  # class Sinclair
+  #   class OptionsParser::Dummy
+  #     include OptionsParser
+  #
+  #     def initialize(options)
+  #       @options = options.deep_dup
+  #     end
+  #   end
+  #
+  #   def the_method
+  #     return 'missing option' if options_object.switch.nil?
+  #
+  #     if options_object.switch
+  #       "The value is #{options_object.option_1}"
+  #     else
+  #       "The value is not #{options_object.option_1} but #{options_object.option_2}"
+  #     end
+  #   end
+  # end
+  module OptionsParser
+    extend ActiveSupport::Concern
 
-  attr_reader :options
+    private
 
-  def options_object
-    @options_object ||= OpenStruct.new options
+    attr_reader :options
+
+    def options_object
+      @options_object ||= OpenStruct.new options
+    end
   end
 end
