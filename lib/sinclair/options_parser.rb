09d288f8365a307424524057b41dae9e05c0856f
darthjee <darthjee@gmail.com>
2018-07-09 14:49:48 +0200

    Fix description

--- lib/sinclair/options_parser.rb
+++ lib/sinclair/options_parser.rb
@@ -2,26 +2,25 @@ class Sinclair
   # Concern for easily adding options
   #
   # @example
+  #   class Sinclair
+  #     class OptionsParser::Dummy
+  #       include OptionsParser
   #
-  # class Sinclair
-  #   class OptionsParser::Dummy
-  #     include OptionsParser
+  #       def initialize(options)
+  #         @options = options.deep_dup
+  #       end
+  #     end
   #
-  #     def initialize(options)
-  #       @options = options.deep_dup
+  #     def the_method
+  #       return 'missing option' if options_object.switch.nil?
+  #
+  #       if options_object.switch
+  #         "The value is #{options_object.option_1}"
+  #       else
+  #         "The value is not #{options_object.option_1} but #{options_object.option_2}"
+  #       end
   #     end
   #   end
-  #
-  #   def the_method
-  #     return 'missing option' if options_object.switch.nil?
-  #
-  #     if options_object.switch
-  #       "The value is #{options_object.option_1}"
-  #     else
-  #       "The value is not #{options_object.option_1} but #{options_object.option_2}"
-  #     end
-  #   end
-  # end
   module OptionsParser
     extend ActiveSupport::Concern
 
