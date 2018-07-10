7f85286c0239375f28682995be331c9c0cb367c7
darthjee <darthjee@gmail.com>
2018-07-10 16:32:50 +0200

    Use ruby &. operator

--- spec/support/models/dummy_builder.rb
+++ spec/support/models/dummy_builder.rb
@@ -4,7 +4,7 @@ class Sinclair
   class DummyBuilder < Sinclair
     def init
       add_method(:blocked) { 1 }
-      add_method(:defined, "@value = value + #{options_object.try(:increment) || 1}")
+      add_method(:defined, "@value = value + #{options_object&.increment || 1}")
       add_method(:value, '@value ||= 0')
     end
   end
