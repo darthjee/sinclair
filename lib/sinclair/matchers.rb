a07f927aa9e07658df8d31e87a8e89210a7796e0
darthjee <darthjee@gmail.com>
2019-03-26 21:18:30 +0100

    Update matchers documentation

--- lib/sinclair/matchers.rb
+++ lib/sinclair/matchers.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Sinclair
+  # @api public
+  #
   # Matchers module will have the DSL to be included in RSpec in order to have
   # access to the matchers
   #
@@ -29,6 +31,9 @@ class Sinclair
     autoload :AddMethodTo, 'sinclair/matchers/add_method_to'
 
     # DSL to AddMethod
+    #
+    # @example (see Sinclair::Matchers)
+    #
     # @return [AddMethod] RSpec Matcher
     def add_method(method)
       Sinclair::Matchers::AddMethod.new(method)
