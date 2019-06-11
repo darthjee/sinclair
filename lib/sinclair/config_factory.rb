e8d3e27e7598a2338e4242a69237fa52b09e2edc
darthjee <darthjee@gmail.com>
2019-06-11 18:13:58 +0200

    Create own config class

--- lib/sinclair/config_factory.rb
+++ lib/sinclair/config_factory.rb
@@ -1,11 +1,17 @@
 class Sinclair
   class ConfigFactory
     def config
-      @config ||= Config.new
+      @config ||= config_class.new
     end
 
     def reset
       @config = nil
+    end
+
+    private
+
+    def config_class
+      @config_class ||= Class.new(Config)
     end
   end
 end
