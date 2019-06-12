58048575ff1584621728496d2b2d5d5f3df0d215
darthjee <darthjee@gmail.com>
2019-06-12 17:21:35 +0200

    Use child factory

--- lib/sinclair/configurable.rb
+++ lib/sinclair/configurable.rb
@@ -2,14 +2,16 @@ class Sinclair
   module Configurable
     delegate :config, :reset, :configure, to: :config_factory
 
+    protected
+
+    def config_factory
+      @config_factory ||= superclass.is_a?(Configurable) ? superclass.config_factory.child : ConfigFactory.new
+    end
+
     private
 
     def configurable_with(*attributes)
       config_factory.add_configs(*attributes)
-    end
-
-    def config_factory
-      @config_factory ||= ConfigFactory.new
     end
   end
 end
