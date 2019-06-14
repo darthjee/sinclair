e5b244c17e034abc8be66fec133eb9b316df820d
darthjee <darthjee@gmail.com>
2019-06-14 09:01:12 +0200

    Add missing spec

---
+++ spec/integration/yard/sinclair/config_builder_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+describe Sinclair::ConfigBuilder do
+  describe 'yard' do
+    describe '#instance_eval' do
+      subject(:builder) do
+        described_class.new(config, :name)
+      end
+
+      let(:config) { MyConfig.new }
+
+      it 'sets variable from config' do
+        expect { builder.instance_eval { |c| c.name 'John' } }
+          .to change(config, :name)
+          .from(nil).to('John')
+      end
+    end
+  end
+end
