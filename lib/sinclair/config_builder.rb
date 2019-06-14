e7d968e905a5798a6c2c7577e6caead1dddf36f1
darthjee <darthjee@gmail.com>
2019-06-14 08:56:29 +0200

    Add config builder documentation

--- lib/sinclair/config_builder.rb
+++ lib/sinclair/config_builder.rb
@@ -1,7 +1,32 @@
 # frozen_string_literal: true
 
 class Sinclair
+  # @api private
+  #
+  # Class responsible for setting the values on configuration
+  #
+  # @example
+  #   class MyConfig
+  #     attr_reader :name, :config
+  #   end
+  #
+  #   config = MyConfig.new
+  #
+  #   builder = Sinclair::ConfigBuilder.new(config, [:name])
+  #
+  #   builder.instance_eval { |c| c.name 'John' }
+  #
+  #   config.name # returns 'John'
   class ConfigBuilder
+    # A new instance of ConfigBuilder
+    #
+    # @param config [Sinclair::Config] config object to be build
+    #   config object has no attribute setters (only readers)
+    #   so all attributes are set as instance variable
+    #
+    # @param config_attributes [Array<Symbol>] list of attributes
+    #   that can be set on config (expecting that config has
+    #   the right attribute readers)
     def initialize(config, config_attributes)
       @config = config
       @config_attributes = config_attributes
@@ -9,6 +34,18 @@ class Sinclair
 
     private
 
+    # @private
+    #
+    # Method called for methods missing
+    #
+    # When a method is missing, it is expected that it is the
+    # name of a variable to be set on config (as long as it was
+    # defined in the config_attributes_array)
+    #
+    # @param method_name [Symbol] name of the method called
+    # @param args [Array<Object>] arguments of the call
+    #
+    # @return [Object]
     def method_missing(method_name, *args)
       return super unless @config_attributes.include?(method_name)
 
