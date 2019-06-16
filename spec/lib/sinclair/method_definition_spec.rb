05ea0d2399db604d7119299ee74cc7a123c08b40
darthjee <darthjee@gmail.com>
2019-06-16 16:45:49 +0200

    Extract shared example

--- spec/lib/sinclair/method_definition_spec.rb
+++ spec/lib/sinclair/method_definition_spec.rb
@@ -2,32 +2,6 @@
 
 require 'spec_helper'
 
-shared_examples 'MethodDefinition#build' do
-  it do
-    expect { method_definition.build(klass) }.to add_method(method_name).to(klass)
-  end
-
-  describe 'after build' do
-    before do
-      method_definition.build(klass)
-    end
-
-    it 'adds the method to the klass instance' do
-      expect(instance).to respond_to(method_name)
-    end
-
-    it 'evaluates return of the method within the instance context' do
-      expect(instance.the_method).to eq(1)
-    end
-
-    it 'evaluates in the context of the instance' do
-      expect { instance.the_method }
-        .to change { instance.instance_variable_get(:@x) }
-        .from(nil).to(1)
-    end
-  end
-end
-
 describe Sinclair::MethodDefinition do
   let(:klass)    { Class.new }
   let(:instance) { klass.new }
