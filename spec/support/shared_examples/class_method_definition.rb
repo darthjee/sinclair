fc2bdd8a3794256246cf7ce621f731f17b4ea194
darthjee <darthjee@gmail.com>
2019-07-18 22:05:03 +0200

    Split shared examples

--- spec/support/shared_examples/class_method_definition.rb
+++ spec/support/shared_examples/class_method_definition.rb
@@ -1,31 +1,5 @@
 # frozen_string_literal: true
 
-shared_examples 'MethodDefinition#build' do
-  it do
-    expect { method_definition.build(klass) }.to add_method(method_name).to(klass)
-  end
-
-  describe 'after build' do
-    before do
-      method_definition.build(klass)
-    end
-
-    it 'adds the method to the klass instance' do
-      expect(instance).to respond_to(method_name)
-    end
-
-    it 'evaluates return of the method within the instance context' do
-      expect(instance.the_method).to eq(1)
-    end
-
-    it 'evaluates in the context of the instance' do
-      expect { instance.the_method }
-        .to change { instance.instance_variable_get(:@x) }
-        .from(nil).to(1)
-    end
-  end
-end
-
 shared_examples 'ClassMethodDefinition#build' do
   it do
     expect { method_definition.build(klass) }
@@ -53,17 +27,6 @@ shared_examples 'ClassMethodDefinition#build' do
   end
 end
 
-
-shared_examples 'MethodDefinition#build without cache' do
-  it_behaves_like 'MethodDefinition#build'
-
-  it 'creates a dynamic method' do
-    method_definition.build(klass)
-    expect { instance.the_method }.to change(instance, :the_method)
-      .from(1).to(3)
-  end
-end
-
 shared_examples 'ClassMethodDefinition#build without cache' do
   it_behaves_like 'ClassMethodDefinition#build'
 
@@ -71,23 +34,6 @@ shared_examples 'ClassMethodDefinition#build without cache' do
     method_definition.build(klass)
     expect { klass.the_method }.to change(klass, :the_method)
       .from(1).to(3)
-  end
-end
-
-
-shared_examples 'MethodDefinition#build with cache' do
-  it_behaves_like 'MethodDefinition#build'
-
-  it 'creates a semi-dynamic method' do
-    method_definition.build(klass)
-    expect { instance.the_method }.not_to change(instance, :the_method)
-  end
-
-  it 'sets the instance variable' do
-    method_definition.build(klass)
-    expect { instance.the_method }
-      .to change { instance.instance_variable_get("@#{method_name}") }
-      .from(nil).to(1)
   end
 end
 
@@ -104,48 +50,6 @@ shared_examples 'ClassMethodDefinition#build with cache' do
     expect { klass.the_method }
       .to change { klass.instance_variable_get("@#{method_name}") }
       .from(nil).to(1)
-  end
-end
-
-shared_examples 'MethodDefinition#build with cache options' do
-  context 'when cached is true' do
-    let(:cached_option) { true }
-
-    it_behaves_like 'MethodDefinition#build with cache'
-
-    context 'when instance variable has been set as nil' do
-      before do
-        method_definition.build(klass)
-        instance.instance_variable_set(:@the_method, nil)
-      end
-
-      it 'returns a new value' do
-        expect(instance.the_method).to eq(1)
-      end
-    end
-  end
-
-  context 'when cached is full' do
-    let(:cached_option) { :full }
-
-    it_behaves_like 'MethodDefinition#build with cache'
-
-    context 'when instance variable has been set as nil' do
-      before do
-        method_definition.build(klass)
-        instance.instance_variable_set(:@the_method, nil)
-      end
-
-      it 'returns always nil' do
-        expect(instance.the_method).to be_nil
-      end
-    end
-  end
-
-  context 'when cached is false' do
-    let(:cached_option) { false }
-
-    it_behaves_like 'MethodDefinition#build without cache'
   end
 end
 
---
+++ spec/support/shared_examples/instance_method_definition.rb
@@ -0,0 +1,96 @@
+# frozen_string_literal: true
+
+shared_examples 'MethodDefinition#build' do
+  it do
+    expect { method_definition.build(klass) }.to add_method(method_name).to(klass)
+  end
+
+  describe 'after build' do
+    before do
+      method_definition.build(klass)
+    end
+
+    it 'adds the method to the klass instance' do
+      expect(instance).to respond_to(method_name)
+    end
+
+    it 'evaluates return of the method within the instance context' do
+      expect(instance.the_method).to eq(1)
+    end
+
+    it 'evaluates in the context of the instance' do
+      expect { instance.the_method }
+        .to change { instance.instance_variable_get(:@x) }
+        .from(nil).to(1)
+    end
+  end
+end
+
+
+shared_examples 'MethodDefinition#build without cache' do
+  it_behaves_like 'MethodDefinition#build'
+
+  it 'creates a dynamic method' do
+    method_definition.build(klass)
+    expect { instance.the_method }.to change(instance, :the_method)
+      .from(1).to(3)
+  end
+end
+
+shared_examples 'MethodDefinition#build with cache' do
+  it_behaves_like 'MethodDefinition#build'
+
+  it 'creates a semi-dynamic method' do
+    method_definition.build(klass)
+    expect { instance.the_method }.not_to change(instance, :the_method)
+  end
+
+  it 'sets the instance variable' do
+    method_definition.build(klass)
+    expect { instance.the_method }
+      .to change { instance.instance_variable_get("@#{method_name}") }
+      .from(nil).to(1)
+  end
+end
+
+shared_examples 'MethodDefinition#build with cache options' do
+  context 'when cached is true' do
+    let(:cached_option) { true }
+
+    it_behaves_like 'MethodDefinition#build with cache'
+
+    context 'when instance variable has been set as nil' do
+      before do
+        method_definition.build(klass)
+        instance.instance_variable_set(:@the_method, nil)
+      end
+
+      it 'returns a new value' do
+        expect(instance.the_method).to eq(1)
+      end
+    end
+  end
+
+  context 'when cached is full' do
+    let(:cached_option) { :full }
+
+    it_behaves_like 'MethodDefinition#build with cache'
+
+    context 'when instance variable has been set as nil' do
+      before do
+        method_definition.build(klass)
+        instance.instance_variable_set(:@the_method, nil)
+      end
+
+      it 'returns always nil' do
+        expect(instance.the_method).to be_nil
+      end
+    end
+  end
+
+  context 'when cached is false' do
+    let(:cached_option) { false }
+
+    it_behaves_like 'MethodDefinition#build without cache'
+  end
+end
