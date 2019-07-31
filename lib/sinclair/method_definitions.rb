29727fcc9c51b7c3575b72997d9d3c5fff2521bd
darthjee <darthjee@gmail.com>
2019-07-31 22:57:02 +0200

    Use enumerator

--- lib/sinclair/method_definitions.rb
+++ lib/sinclair/method_definitions.rb
@@ -1,24 +1,9 @@
 # frozen_string_literal: true
 
 class Sinclair
-  class MethodDefinitions
-    delegate :each, to: :definitions
-
+  class MethodDefinitions < Array
     def add(definition_class, name, code = nil, **options, &block)
-      definitions << definition_class.from(name, code, **options, &block)
-    end
-
-    private
-
-    # @private
-    #
-    # @api private
-    #
-    # List of mthod definitions
-    #
-    # @return [Array<MethodDefinition>]
-    def definitions
-      @definitions ||= []
+      self << definition_class.from(name, code, **options, &block)
     end
   end
 end
