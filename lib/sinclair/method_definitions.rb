9cf84a5f35f1e8caa24b08c5d3a4a9b34ac2fbf1
darthjee <darthjee@gmail.com>
2019-08-02 15:41:04 +0200

    Rubocop fix

--- lib/sinclair/method_definitions.rb
+++ lib/sinclair/method_definitions.rb
@@ -3,7 +3,6 @@
 class Sinclair
   # Enumerator holding all method definitions
   class MethodDefinitions < Array
-
     # Builds and adds new definition
     def add(definition_class, name, code = nil, **options, &block)
       self << definition_class.from(name, code, **options, &block)
