e1514a4e6adc437b681ee4adae14e6e21abe7ff8
darthjee <darthjee@gmail.com>
2019-08-09 18:13:33 +0200

    Extract string builder

--- lib/sinclair/method_builder.rb
+++ lib/sinclair/method_builder.rb
@@ -1,29 +1,25 @@
 class Sinclair
   class MethodBuilder
+    autoload :StringMethodBuilder, 'sinclair/method_builder/string_method_builder'
+
     def initialize(klass)
       @klass = klass
     end
 
     def build_method(definition)
-      code_definition = <<-CODE
-        def #{definition.name}
-          #{definition.code_line}
-        end
-      CODE
-      klass.module_eval(code_definition, __FILE__, __LINE__ + 1)
+      string_method_builder.build_method(definition)
     end
 
     def build_class_method(definition)
-      code_definition = <<-CODE
-        def self.#{definition.name}
-          #{definition.code_line}
-        end
-      CODE
-      klass.module_eval(code_definition, __FILE__, __LINE__ + 1)
+      string_method_builder.build_class_method(definition)
     end
 
     private
 
     attr_reader :klass
+
+    def string_method_builder
+      @string_method_builder ||= StringMethodBuilder.new(klass)
+    end
   end
 end
---
+++ lib/sinclair/method_builder/string_method_builder.rb
@@ -0,0 +1,31 @@
+class Sinclair
+  class MethodBuilder
+    class StringMethodBuilder
+      def initialize(klass)
+        @klass = klass
+      end
+
+      def build_method(definition)
+        code_definition = <<-CODE
+        def #{definition.name}
+          #{definition.code_line}
+        end
+        CODE
+        klass.module_eval(code_definition, __FILE__, __LINE__ + 1)
+      end
+
+      def build_class_method(definition)
+        code_definition = <<-CODE
+        def self.#{definition.name}
+          #{definition.code_line}
+        end
+        CODE
+        klass.module_eval(code_definition, __FILE__, __LINE__ + 1)
+      end
+
+      private
+
+      attr_reader :klass
+    end
+  end
+end
