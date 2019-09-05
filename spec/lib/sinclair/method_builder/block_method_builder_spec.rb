7029b73405888e7b85e353a18e1845e61e87cc97
darthjee <darthjee@gmail.com>
2019-09-05 08:54:04 +0200

    Add block builder spec

---
+++ spec/lib/sinclair/method_builder/block_method_builder_spec.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Sinclair::MethodBuilder::BlockMethodBuilder do
+  describe '#build' do
+    subject(:builder) do
+      described_class.new(klass, definition, type: type)
+    end
+
+    let(:klass)       { Class.new }
+    let(:instance)    { klass.new }
+    let(:value)       { Random.rand }
+    let(:method_name) { :the_method }
+
+    let(:definition) do
+      result = value
+      Sinclair::MethodDefinition.from(method_name) { result }
+    end
+
+    context 'when type is instance' do
+      let(:type) { Sinclair::MethodBuilder::INSTANCE_METHOD }
+
+      it do
+        expect { builder.build }
+          .to add_method(method_name).to(instance)
+      end
+
+      context 'when the method is built' do
+        before { builder.build }
+
+        it 'returns the result of the block when called' do
+          expect(instance.the_method).to eq(value)
+        end
+      end
+    end
+
+    context 'when type is class' do
+      let(:type) { Sinclair::MethodBuilder::CLASS_METHOD }
+
+      it do
+        expect { builder.build }
+          .to add_class_method(method_name).to(klass)
+      end
+
+      context 'when the method is built' do
+        before { builder.build }
+
+        it 'returns the result of the block when called' do
+          expect(klass.the_method).to eq(value)
+        end
+      end
+    end
+  end
+end
