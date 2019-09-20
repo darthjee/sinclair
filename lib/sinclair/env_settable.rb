1884d51cd6b3868de0d37072183c873bed4e9ce8
darthjee <darthjee@gmail.com>
2019-09-20 17:02:51 +0200

    Adds more documentation

--- lib/sinclair/env_settable.rb
+++ lib/sinclair/env_settable.rb
@@ -28,10 +28,36 @@ class Sinclair
   module EnvSettable
     autoload :Builder, 'sinclair/env_settable/builder'
 
+    private
+
+    # @private
+    # @api public
+    # @visibility public
+    #
+    # Sets environment keys prefix
+    #
+    # @param prefix [String] prefix of the env keys
+    #
+    # @return [String]
+    #
+    # @example (see EnvSettable)
     def settings_prefix(prefix)
       @settings_prefix = prefix
     end
 
+    # @private
+    # @api public
+    # @visibility public
+    #
+    # Adds settings
+    #
+    # @param settings_name [Array<Symbol,String>] Name of all settings
+    #   to be added
+    # @param defaults [Hash] Settings with default values
+    #
+    # @return (see Sinclair#build)
+    #
+    # @example (see EnvSettable)
     def with_settings(*settings_name, **defaults)
       Builder.new(self, @settings_prefix, *settings_name, **defaults).build
     end
