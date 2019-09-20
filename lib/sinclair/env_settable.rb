b7c70f17fdc7b189781849e22c2f51e36c9f8255
darthjee <darthjee@gmail.com>
2019-09-20 16:54:26 +0200

    Add documentation

--- lib/sinclair/env_settable.rb
+++ lib/sinclair/env_settable.rb
@@ -1,6 +1,30 @@
 # frozen_string_literal: true
 
 class Sinclair
+  # @api public
+  # @author darthjee
+  #
+  # module to be extended allowing configurations from environment
+  #
+  # @example
+  #   class MyAppClient
+  #     extend Sinclair::EnvSettable
+  #
+  #     settings_prefix 'MY_APP'
+  #
+  #     with_settings :username, :password, host: 'my-host.com'
+  #   end
+  #
+  #   ENV['MY_APP_USERNAME'] = 'my_login'
+  #
+  #   MyAppClient.username # returns 'my_login'
+  #   MyAppClient.password # returns nil
+  #   MyAppClient.host     # returns 'my-host.com'
+  #
+  #   ENV['MY_APP_HOST'] = 'other-host.com'
+  #
+  #   MyAppClient.host     # returns 'other-host.com'
+  #
   module EnvSettable
     autoload :Builder, 'sinclair/env_settable/builder'
 
