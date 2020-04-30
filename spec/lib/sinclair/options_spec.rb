7c10c34fe878efd8f013425696ffaeeec2bdf6dd
darthjee <darthjee@gmail.com>
2020-04-30 16:37:42 +0200

    Add specs for subclass

--- spec/lib/sinclair/options_spec.rb
+++ spec/lib/sinclair/options_spec.rb
@@ -5,22 +5,22 @@ require 'spec_helper'
 describe Sinclair::Options do
   subject(:options) { klass.new }
 
-  let(:klass) { Class.new(described_class) }
-
   describe '.with_options' do
+    let(:klass) { Class.new(described_class) }
+
     context 'when calling with keys' do
       it 'add first method' do
-        expect { klass.send(:with_options, :timeout, :retries) }
+        expect { klass.send(:with_options, :timeout, 'retries') }
           .to add_method(:timeout).to(klass)
       end
 
       it 'add second method' do
-        expect { klass.send(:with_options, :timeout, :retries) }
+        expect { klass.send(:with_options, :timeout, 'retries') }
           .to add_method(:retries).to(klass)
       end
 
       context 'when when calling method after building' do
-        before { klass.send(:with_options, :timeout, :retries) }
+        before { klass.send(:with_options, :timeout, 'retries') }
 
         it { expect(options.timeout).to be_nil }
       end
@@ -28,14 +28,33 @@ describe Sinclair::Options do
 
     context 'when calling with a hash' do
       it 'adds method' do
-        expect { klass.send(:with_options, timeout: 10) }
+        expect { klass.send(:with_options, timeout: 10, 'retries' => 20) }
           .to add_method(:timeout).to(klass)
       end
 
       context 'when when calling method after building' do
-        before { klass.send(:with_options, timeout: 10) }
+        before { klass.send(:with_options, timeout: 10, 'retries' => 20) }
 
-        it { expect(options.timeout).not_to be_nil }
+        it 'returns default value' do
+          expect(options.retries).to eq(20)
+        end
+      end
+    end
+
+    context 'when calling on subclass' do
+      let(:supercllass) { Class.new(described_class) }
+      let(:klass)       { Class.new(superclass) }
+
+      before { superclass.send(:with_options, :timeout, 'retries') }
+
+      it 'add first method' do
+        expect { klass.send(:with_options, :protocol, 'port' => 443) }
+          .to add_method(:protocol).to(klass)
+      end
+
+      it 'add second method' do
+        expect { klass.send(:with_options, :protocol, 'port' => 443) }
+          .to add_method(:port).to(klass)
       end
     end
   end
@@ -74,6 +93,43 @@ describe Sinclair::Options do
 
       it 'does not mess with non initialized attributes with defaults' do
         expect(options.port).to eq(443)
+      end
+    end
+
+    context 'when initializing subclass with valid args' do
+      subject(:options) do
+        klass.new(timeout: timeout, protocol: 'http')
+      end
+
+      let(:klass)   { Class.new(ConnectionOptions) }
+      let(:timeout) { Random.rand(10..19) }
+
+      before do
+        klass.send(:with_options, :access_token, identifier: 'my client')
+      end
+
+      it 'sets value of options attribute' do
+        expect(options.timeout).to eq(timeout)
+      end
+
+      it 'sets value of options attribute that has default' do
+        expect(options.protocol).to eq('http')
+      end
+
+      it 'does not mess with non initialized attributes' do
+        expect(options.retries).to be_nil
+      end
+
+      it 'does not mess with non initialized attributes with defaults' do
+        expect(options.port).to eq(443)
+      end
+
+      it 'returns nil for new added option without default' do
+        expect(options.access_token).to be_nil
+      end
+
+      it 'returns value for new added option with default' do
+        expect(options.identifier).to eq('my client')
       end
     end
 
