94185c044dfc591dadc8a95967ae628199db6b09
darthjee <darthjee@gmail.com>
2020-05-22 01:51:51 +0200

    Extract class methods

--- lib/sinclair/options.rb
+++ lib/sinclair/options.rb
@@ -20,98 +20,10 @@ class Sinclair
   #   options.port     # returns 8080
   #   options.protocol # returns 'https'
   class Options
-    autoload :Builder, 'sinclair/options/builder'
+    autoload :Builder,      'sinclair/options/builder'
+    autoload :ClassMethods, 'sinclair/options/class_methods'
 
-    class << self
-      # @api private
-      #
-      # returns invalid options
-      #
-      # @return [Array<Symbol>]
-      def invalid_options_in(names)
-        names.map(&:to_sym) - allowed_options.to_a
-      end
-
-      # @api private
-      #
-      # Allow new option
-      #
-      # This does not create the method
-      #
-      # @param name [String,Symbol] options to be allowed
-      #
-      # @return [Set<Symbol>]
-      def allow(name)
-        allowed_options << name.to_sym
-      end
-
-      # @api private
-      # @private
-      #
-      # Options allowed when initializing options
-      #
-      # @return [Set<Symbol>]
-      def allowed_options
-        @allowed_options ||= superclass.try(:allowed_options).dup || Set.new
-      end
-
-      # @api private
-      #
-      # checks if class skips initialization validation
-      #
-      # @return [TrueClass,FalseClass]
-      def skip_validation?
-        @skip_validation ||= superclass.try(:skip_validation?) || false
-      end
-
-      private
-
-      # @api public
-      # @!visibility public
-      #
-      # Add available options
-      #
-      # @example (see Options)
-      #
-      # @return (see Sinclair#build)
-      #
-      # @overload with_options(*options)
-      #   @param options [Array<Symbol>] list of accepted
-      #     options
-      # @overload with_options(*options, **defaults)
-      #   @param options [Array<Symbol>] list of accepted
-      #     options
-      #   @param defaults [Hash<Symbol,Object>] default options
-      #     hash
-      def with_options(*options)
-        Builder.new(self, *options).build
-      end
-
-      # @api public
-      # @!visibility public
-      #
-      # Changes class to skip attributes validation
-      #
-      # when initializing options, options
-      # will accept any arguments when validation
-      # is skipped
-      #
-      # @return [TrueClass]
-      #
-      # @example
-      #   class BuilderOptions < Sinclair::Options
-      #     with_options :name
-      #
-      #     skip_validation
-      #   end
-      #   options = BuilderOptions.new(name: 'Joe', age: 10)
-      #
-      #   options.name      # returns 'Joe'
-      #   options.try(:age) # returns nil
-      def skip_validation
-        @skip_validation = true
-      end
-    end
+    extend ClassMethods
 
     # @param options [Hash] hash with options (see {.options}, {.with_options})
     # @example (see Options)
