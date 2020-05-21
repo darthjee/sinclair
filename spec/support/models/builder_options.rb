d70b47f6dccdc7d8ab863a0f8b52fe4fcc23a8f2
darthjee <darthjee@gmail.com>
2020-05-22 01:52:03 +0200

    Add missing spec files

---
+++ spec/support/models/builder_options.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class BuilderOptions < Sinclair::Options
+  with_options :name
+
+  skip_validation
+end
---
+++ spec/support/models/open_options.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class OpenOptions < Sinclair::Options
+  with_options :valid_option
+
+  skip_validation
+end
