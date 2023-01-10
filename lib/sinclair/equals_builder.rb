a4feaa68272c759dd42844a174da3940c068e7fa
darthjee <darthjee@gmail.com>
2023-01-10 14:31:00 -0300

    Make it public

--- lib/sinclair/equals_builder.rb
+++ lib/sinclair/equals_builder.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Sinclair
-  # @api protected
+  # @api public
   # @author darthjee
   #
   # Class responsible for checking if two instances of a class are the equals
