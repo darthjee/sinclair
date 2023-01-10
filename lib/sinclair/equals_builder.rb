9bf4a0a9d23eac931b056c311a2a55b6f3b73e2b
darthjee <darthjee@gmail.com>
2023-01-10 14:03:52 -0300

    Add some documentation

--- lib/sinclair/equals_builder.rb
+++ lib/sinclair/equals_builder.rb
@@ -3,13 +3,25 @@
 class Sinclair
   # @api protected
   # @author darthjee
+  #
+  # Class responsible for checking if two instances of a class are the equals
   class EqualsBuilder
     attr_reader :attributes
 
+    # @param attributes [Array<Symbol,String>] list of relevant attributes
     def initialize(attributes)
       @attributes = attributes
     end
 
+    # Returns if 2 objects are equals
+    #
+    # the check takes into consideration
+    #   - They should be instances of the same class
+    #   - Their attributes (method calls) should return the same value,
+    #   for the configured attributes
+    #   - Public and private attributes are checked
+    #
+    # @return  [TrueClass,FalseClass]
     def match?(model, other)
       return false unless model.class == other.class
 
