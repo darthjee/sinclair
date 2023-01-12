9c02e2c13de7a9d7c77574b923deb2220f423559
darthjee <darthjee@gmail.com>
2023-01-11 21:47:21 -0300

    Rubocop fix

--- spec/integration/yard/sinclair/comparable_spec.rb
+++ spec/integration/yard/sinclair/comparable_spec.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
-describe Sinclair::EqualsChecker do
+describe Sinclair::Comparable do
   describe '#yard' do
     describe '.comparable_by?' do
       let(:model_class) do
