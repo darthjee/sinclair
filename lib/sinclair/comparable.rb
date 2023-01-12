26fcd874f028a27f1392124c90574a4cf61527ac
darthjee <darthjee@gmail.com>
2023-01-11 21:22:13 -0300

    Add docs

--- lib/sinclair/comparable.rb
+++ lib/sinclair/comparable.rb
@@ -5,9 +5,17 @@ require 'sinclair/comparable/class_methods'
 class Sinclair
   # @api public
   # @author darthjee
+  #
+  # Concern to be added on classes for easy +==+ comparison
   module Comparable
     extend ActiveSupport::Concern
 
+    # Checks if an instance of a comparable is equals another
+    #
+    # @param other [Object] an object that should be equal to comparable
+    #
+    # @return [Boolean]
+    # @example (see Sinclair::Comparable)
     def ==(other)
       self.class.equals_checker.match?(self, other)
     end
--- lib/sinclair/comparable/class_methods.rb
+++ lib/sinclair/comparable/class_methods.rb
@@ -2,11 +2,28 @@
 
 class Sinclair
   module Comparable
+    # @api public
+    # @author darthjee
+    #
+    # Class methods of {Sinclair::Comparable}
+    #
+    # @example (see Sinclair::Comparable)
     module ClassMethods
+      # Adds fields to the comparison algorythim
+      #
+      # @param attributes [Array<Symbol>] attributes to be added to comparison
+      #
+      # @see Sinclair::EqualsChecker
+      # @example (see Sinclair::Comparable)
       def comparable_by(*attributes)
         equals_checker.add(*attributes)
       end
 
+      # @api private
+      # 
+      # Returns a comparable configured for the class
+      #
+      # @return [Sinclair::EqualsChecker]
       def equals_checker
         @equals_checker ||= Sinclair::EqualsChecker.new
       end
