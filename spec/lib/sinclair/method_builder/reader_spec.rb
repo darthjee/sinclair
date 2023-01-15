05f8a0e47c9c2d8e9d0664b0f6031d0930622cf3
darthjee <darthjee@gmail.com>
2023-01-15 12:59:19 -0300

    Add negative checks

--- spec/lib/sinclair/method_builder/reader_spec.rb
+++ spec/lib/sinclair/method_builder/reader_spec.rb
@@ -8,6 +8,26 @@ describe Sinclair::MethodBuilder::Reader do
       described_class.new(klass, *attributes, type: type)
     end
 
-    it_behaves_like 'a method builder that adds attribute reader'
+    it_behaves_like 'a method builder that adds attribute reader' do
+      context 'when type is instance' do
+        let(:type) { Sinclair::MethodBuilder::INSTANCE_METHOD }
+
+        it 'does not add a reader' do
+          expect { builder.build }
+            .not_to add_method("#{method_name}=")
+            .to(instance)
+        end
+      end
+
+      context 'when type is class' do
+        let(:type) { Sinclair::MethodBuilder::CLASS_METHOD }
+
+        it 'does not add a reader' do
+          expect { builder.build }
+            .not_to add_class_method("#{method_name}=")
+            .to(klass)
+        end
+      end
+    end
   end
 end
