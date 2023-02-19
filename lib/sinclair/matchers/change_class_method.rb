f3526a1094ab757fd4c0193425a206d45bcc658b
darthjee <darthjee@gmail.com>
2023-02-19 12:41:10 -0300

    WIP: organize doc for #to

--- lib/sinclair/matchers/change_class_method.rb
+++ lib/sinclair/matchers/change_class_method.rb
@@ -7,37 +7,43 @@ class Sinclair
     #
     # AddInstanceMethod is able to build an instance of
     # {Sinclair::Matchers::ChangeClassMethodOn}
+    #
+    # @example
+    #  RSpec.configure do |config|
+    #    config.include Sinclair::Matchers
+    #  end
+    #
+    #  class MyModel
+    #  end
+    #
+    #  RSpec.describe 'my test' do
+    #    let(:builder) { Sinclair.new(klass) }
+    #    let(:klass)   { Class.new(MyModel) }
+    #
+    #    before do
+    #      builder.add_class_method(:the_method) { 10 }
+    #      builder.build
+    #      builder.add_class_method(:the_method) { 20 }
+    #    end
+    #
+    #    it do
+    #      expect{ builder.build }.to change_class_method(:the_method).on(klass)
+    #    end
+    #  end
+
     class ChangeClassMethod < Base
       include AddMethod
 
+      # @method to(target = nil)
       # @api public
       #
-      # Builds final matcher
+      # The matcher checks if a method was added
+      # to a class or instance
       #
-      # @return [Sinclair::Matchers::ChangeClassMethodOn]
+      # @param (see AddMethod#to)
       #
-      # @example
-      #  RSpec.configure do |config|
-      #    config.include Sinclair::Matchers
-      #  end
-      #
-      #  class MyModel
-      #  end
-      #
-      #  RSpec.describe 'my test' do
-      #    let(:builder) { Sinclair.new(klass) }
-      #    let(:klass)   { Class.new(MyModel) }
-      #
-      #    before do
-      #      builder.add_class_method(:the_method) { 10 }
-      #      builder.build
-      #      builder.add_class_method(:the_method) { 20 }
-      #    end
-      #
-      #    it do
-      #      expect{ builder.build }.to change_class_method(:the_method).on(klass)
-      #    end
-      #  end
+      # @return [ChangeClassMethodOn]
+      
       alias on to
 
       private
