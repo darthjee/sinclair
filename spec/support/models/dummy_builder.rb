12f56940f538c35d4cab4aa6e09fa2e64a295a96
darthjee <darthjee@gmail.com>
2023-02-24 10:31:19 -0300

    Use cached for test

--- spec/support/models/dummy_builder.rb
+++ spec/support/models/dummy_builder.rb
@@ -5,7 +5,7 @@ class Sinclair
     def init
       add_method(:blocked) { 1 }
       add_method(:defined, "@value = value + #{options_object&.increment || 1}")
-      add_method(:value, '@value ||= 0')
+      add_method(:value, cached: true) { 0 }
       add_method(:type_block, type: :block) { 3 }
       add_method(:attr_accessor, :some_attribute, type: :call)
     end
