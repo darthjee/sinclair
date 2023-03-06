afed17f3e05a2a0b48be71e1147553766c6763cf
darthjee <darthjee@gmail.com>
2023-03-06 19:33:04 -0300

    Reorganize doc

--- lib/sinclair/method_definitions.rb
+++ lib/sinclair/method_definitions.rb
@@ -8,6 +8,35 @@ class Sinclair
   class MethodDefinitions
     delegate :each, to: :definitions
 
+    # Builds and adds new definition
+    #
+    # The type is decided based in the arguments
+    #
+    # @overload new_add_definition(definition_class, name, code = nil, **options)
+    #   @param name [String,Symbol] method name
+    #   @param code [String] code to be evaluated when the method is ran
+    #   @param options [Hash] Options of construction
+    #   @option options cached [Boolean] Flag telling to create
+    #     a method with cache
+    #
+    # @overload new_add_definition(definition_class, name, **options, &block)
+    #   @param name [String,Symbol] method name
+    #   @param options [Hash] Options of construction
+    #   @option options cached [Boolean] Flag telling to create
+    #     a method with cache
+    #   @param block [Proc]  block to be ran as method
+    #
+    # @overload new_add_definition(type, *args, **options, &block)
+    #   @param type [Symbol] type of definition
+    #     - :string -> {MethodDefinition::StringDefinition}
+    #     - :block -> {MethodDefinition::BlockDefinition}
+    #     - :call -> {MethodDefinition::CallDefinition}
+    #   @param options [Hash] Options of construction
+    #   @option options cached [Boolean] Flag telling to create
+    #     a method with cache
+    #   @param block [Proc]  block to be ran as method
+    #
+    # @return [Array<MethodDefinition>]
     def new_add_definition(*args, type: nil, **options, &block)
       if type
         add_definition(type, *args, **options, &block)
@@ -18,38 +47,20 @@ class Sinclair
 
     private
 
+    # @private
     # Builds and adds new definition
     #
     # The type is decided based in the arguments
-    #
-    # @param name [String,Symbol] method name
-    # @param options [Hash] Options of construction
-    # @option options cached [Boolean] Flag telling to create
-    #   a method with cache
-    #
-    # @overload add(definition_class, name, code = nil, **options)
-    #   @param code [String] code to be evaluated when the method is ran
-    #
-    # @overload add(definition_class, name, **options, &block)
-    #   @param block [Proc]  block to be ran as method
     #
     # @return [Array<MethodDefinition>]
     def add(name, code = nil, **options, &block)
       definitions << MethodDefinition.from(name, code, **options, &block)
     end
 
+    # @private
     # Builds and adds new definition
     #
     # The type is decided based on the argument +type+
-    #
-    # @param type [Symbol] type of definition
-    #   - :string -> {MethodDefinition::StringDefinition}
-    #   - :block -> {MethodDefinition::BlockDefinition}
-    #   - :call -> {MethodDefinition::CallDefinition}
-    # @param options [Hash] Options of construction
-    # @option options cached [Boolean] Flag telling to create
-    #   a method with cache
-    # @param block [Proc]  block to be ran as method
     #
     # @return [Array<MethodDefinition>]
     def add_definition(type, *args, **options, &block)
