345da154cfafd2b12334075b4726f0fbf79a2194
darthjee <darthjee@gmail.com>
2023-03-06 19:23:02 -0300

    Update spec

--- spec/lib/sinclair/method_definitions_spec.rb
+++ spec/lib/sinclair/method_definitions_spec.rb
@@ -5,66 +5,64 @@ require 'spec_helper'
 describe Sinclair::MethodDefinitions do
   subject(:definitions) { described_class.new }
 
-  describe '#add' do
+  describe '#new_add_definition' do
     let(:name) { :the_method }
 
     context 'when passing block' do
       it 'returns the resulting array' do
-        expect(definitions.add(name) { RandomGenerator.rand })
+        expect(definitions.new_add_definition(name) { RandomGenerator.rand })
           .to be_a(Array)
       end
 
       it 'returns an array of MethodDefinition' do
-        expect(definitions.add(name) { RandomGenerator.rand }.first)
+        expect(definitions.new_add_definition(name) { RandomGenerator.rand }.first)
           .to be_a(Sinclair::MethodDefinition)
       end
 
       it 'creates a new BlockDefinition' do
-        expect(definitions.add(name) { RandomGenerator.rand }.first)
+        expect(definitions.new_add_definition(name) { RandomGenerator.rand }.first)
           .to be_a(Sinclair::MethodDefinition::BlockDefinition)
       end
     end
 
     context 'when passing string' do
       it 'returns the resulting array' do
-        expect(definitions.add(name, 'RandomGenerator.rand'))
+        expect(definitions.new_add_definition(name, 'RandomGenerator.rand'))
           .to be_a(Array)
       end
 
       it 'returns an array of MethodDefinition' do
-        expect(definitions.add(name, 'RandomGenerator.rand').last)
+        expect(definitions.new_add_definition(name, 'RandomGenerator.rand').last)
           .to be_a(Sinclair::MethodDefinition)
       end
 
       it 'creates a new StringDefinition' do
-        expect(definitions.add(name, 'RandomGenerator.rand').last)
+        expect(definitions.new_add_definition(name, 'RandomGenerator.rand').last)
           .to be_a(Sinclair::MethodDefinition::StringDefinition)
       end
     end
-  end
 
-  describe '#add_definition' do
     context 'when there are no options nor block' do
       let(:type)      { :call }
       let(:arguments) { %i[attr_reader some_attribute other_attribute] }
 
       it do
-        expect(definitions.add_definition(type, *arguments))
+        expect(definitions.new_add_definition(*arguments, type: type))
           .to be_a(Array)
       end
 
       it 'creates a new definition' do
-        expect(definitions.add_definition(type, *arguments).last)
+        expect(definitions.new_add_definition(*arguments, type: type).last)
           .to be_a(Sinclair::MethodDefinition)
       end
 
       it 'creates a new definition of the chosen type' do
-        expect(definitions.add_definition(type, *arguments).last)
+        expect(definitions.new_add_definition(*arguments, type: type).last)
           .to be_a(Sinclair::MethodDefinition::CallDefinition)
       end
 
       it 'initializes it correctly' do
-        expect(definitions.add_definition(type, *arguments).last.code_string)
+        expect(definitions.new_add_definition(*arguments, type: type).last.code_string)
           .to eq('attr_reader :some_attribute, :other_attribute')
       end
     end
@@ -75,22 +73,22 @@ describe Sinclair::MethodDefinitions do
       let(:block)       { proc { 10 } }
 
       it do
-        expect(definitions.add_definition(type, method_name, &block))
+        expect(definitions.new_add_definition(type, method_name, &block))
           .to be_a(Array)
       end
 
       it 'creates a new definition' do
-        expect(definitions.add_definition(type, method_name, &block).last)
+        expect(definitions.new_add_definition(type, method_name, &block).last)
           .to be_a(Sinclair::MethodDefinition)
       end
 
       it 'creates a new definition of the chosen type' do
-        expect(definitions.add_definition(type, method_name, &block).last)
+        expect(definitions.new_add_definition(type, method_name, &block).last)
           .to be_a(Sinclair::MethodDefinition::BlockDefinition)
       end
 
       it 'initializes it correctly' do
-        expect(definitions.add_definition(type, method_name, &block).last.name)
+        expect(definitions.new_add_definition(method_name, type: type, &block).last.name)
           .to eq(method_name)
       end
     end
@@ -101,22 +99,22 @@ describe Sinclair::MethodDefinitions do
       let(:code)        { '10' }
 
       it do
-        expect(definitions.add_definition(type, method_name, code))
+        expect(definitions.new_add_definition(method_name, code, type: type))
           .to be_a(Array)
       end
 
       it 'creates a new definition' do
-        expect(definitions.add_definition(type, method_name, code).last)
+        expect(definitions.new_add_definition(method_name, code, type: type).last)
           .to be_a(Sinclair::MethodDefinition)
       end
 
       it 'creates a new definition of the chosen type' do
-        expect(definitions.add_definition(type, method_name, code).last)
+        expect(definitions.new_add_definition(method_name, code, type: type).last)
           .to be_a(Sinclair::MethodDefinition::StringDefinition)
       end
 
       it 'initializes it correctly' do
-        expect(definitions.add_definition(type, method_name, code).last.name)
+        expect(definitions.new_add_definition(method_name, code, type: type).last.name)
           .to eq(method_name)
       end
     end
