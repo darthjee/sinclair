93fd2704daa78ef87cd4b541f4981e83228a6226
darthjee <darthjee@gmail.com>
2023-03-06 18:59:27 -0300

    Add integration specs

--- spec/integration/yard/sinclair/add_class_method_spec.rb
+++ spec/integration/yard/sinclair/add_class_method_spec.rb
@@ -26,4 +26,26 @@ describe 'yard Sinclair#add_class_method' do
       expect(klass.timeout).to eq('300')
     end
   end
+
+  describe 'Passing type block' do
+    it 'creates new method' do
+      builder = Sinclair.new(klass)
+      builder.add_class_method(:timeout, type: :block) { ENV['TIMEOUT'] }
+      builder.build
+      ENV['TIMEOUT'] = '300'
+
+      expect(klass.timeout).to eq('300')
+    end
+  end
+
+  describe 'Passing type call' do
+    it 'creates new method' do
+      builder = Sinclair.new(klass)
+      builder.add_class_method(:attr_accessor, :timeout, type: :call)
+      builder.build
+
+      klass.timeout = 10
+      expect(klass.timeout).to eq(10)
+    end
+  end
 end
