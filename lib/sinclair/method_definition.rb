4d94a73304ec76f74fecc54523890f066ada2e97
darthjee <darthjee@gmail.com>
2023-03-07 20:51:36 -0300

    Fix doc

--- lib/sinclair/method_definition.rb
+++ lib/sinclair/method_definition.rb
@@ -59,7 +59,7 @@ class Sinclair
       # The creation is based on type which will be used to infer
       # which subclass of {Sinclair::MethodDefinition} to be used
       #
-      # If type is +nil+ then call is delegated to {#for} which will infer the type
+      # If type is +nil+ then call is delegated to {.from} which will infer the type
       # from the arguments
       #
       # @param type [Symbol] the method definition type
