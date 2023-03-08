9ca760434133a879d4e5c07bacbc28b15b44bd82
darthjee <darthjee@gmail.com>
2023-03-07 21:27:55 -0300

    Add definition spec

---
+++ spec/lib/sinclair/method_definition/new_call_definition_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Sinclair::MethodDefinition::NewCallDefinition do
+  subject(:definition) do
+    described_class.new(call_name, *attributes)
+  end
+
+  let(:call_name)  { :method_call }
+  let(:attributes) { %i[key1 value2] }
+
+  describe '#code_block' do
+    let(:instance) { klass.new }
+    let(:klass) do
+      Class.new do
+        def method_call(*args)
+          args
+        end
+      end
+    end
+
+    it do
+      expect(definition.code_block)
+        .to be_a(Proc)
+    end
+
+    it 'returns a proc with the method call' do
+      expect(instance.instance_eval(&(definition.code_block)))
+        .to eq(attributes)
+    end
+  end
+end
