7b936e6fc2a5ba71a0c56388acd4930b0d8f1166
darthjee <darthjee@gmail.com>
2023-03-10 15:57:57 -0300

    Remove uneeded method

--- lib/sinclair/method_builder/string_method_builder.rb
+++ lib/sinclair/method_builder/string_method_builder.rb
@@ -19,18 +19,6 @@ class Sinclair
 
       private
 
-      # @private
-      #
-      # string used for method name definition
-      #
-      # the string changes depending if it is
-      # a class or instance method
-      #
-      # @return [String]
-      def definition_name
-        instance? ? name : "self.#{name}"
-      end
-
       # string with the code to be defined
       #
       # @return [String]
