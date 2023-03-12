56845b5a52750d407d71f28136ae4efd9c79022f
darthjee <darthjee@gmail.com>
2023-03-12 18:30:05 -0300

    Module becomes a class

--- lib/sinclair/method_definition/parameter_helper.rb
+++ lib/sinclair/method_definition/parameter_helper.rb
@@ -8,38 +8,52 @@ class Sinclair
     # Helper containing helepr methods for remapping parameters
     #
     # @see ParameterBuilder
-    module ParameterHelper
-      class << self
-        def parameters_from(parameters, addtion='', &map_block)
-          parameters_for(*parameters) do |param|
-            "#{param}#{addtion}"
-          end +
-          parameteres_defaults_for(*parameters, &map_block)
-        end
+    class ParameterHelper
+      def self.parameters_from(*args, &block)
+        new(*args, &block).parameters_strings
+      end
 
-        # Maps an array of parameters into an Array of +Strings+
-        #
-        # The code takes advantage of ruby splitting parameters and named
-        # parameters (_defaults) from a list of arguments on method call
-        #
-        # @return [Array<String>]
-        def parameters_for(*parameters, **_defaults, &block)
-          return [] unless parameters
+      private_class_method :new
 
-          parameters.map(&block)
-        end
+      attr_reader :parameters, :addtion, :map_block
 
-        # Maps a hash of defaults parameters into an Array of +Strings+
-        #
-        # The code takes advantage of ruby splitting parameters and named
-        # parameters (_defaults) from a list of arguments on method call
-        #
-        # @return [Array<String>]
-        def parameteres_defaults_for(*_parameters, **defaults, &block)
-          return [] unless defaults
+      def initialize(parameters, addtion='', &map_block)
+        @parameters = parameters
+        @addtion = addtion
+        @map_block = map_block
+      end
 
-          defaults.map(&block)
-        end
+      def parameters_strings
+        parameters_for(*parameters) do |param|
+          "#{param}#{addtion}"
+        end +
+        parameteres_defaults_for(*parameters, &map_block)
+      end
+
+      private
+
+      # Maps an array of parameters into an Array of +Strings+
+      #
+      # The code takes advantage of ruby splitting parameters and named
+      # parameters (_defaults) from a list of arguments on method call
+      #
+      # @return [Array<String>]
+      def parameters_for(*parameters, **_defaults, &block)
+        return [] unless parameters
+
+        parameters.map(&block)
+      end
+
+      # Maps a hash of defaults parameters into an Array of +Strings+
+      #
+      # The code takes advantage of ruby splitting parameters and named
+      # parameters (_defaults) from a list of arguments on method call
+      #
+      # @return [Array<String>]
+      def parameteres_defaults_for(*_parameters, **defaults, &block)
+        return [] unless defaults
+
+        defaults.map(&block)
       end
     end
   end
