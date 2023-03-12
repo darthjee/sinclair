01aead0483217fb6d057f25c13e345161d9df655
darthjee <darthjee@gmail.com>
2023-03-12 17:24:33 -0300

    Update doc

--- lib/sinclair/method_definition/parameter_helper.rb
+++ lib/sinclair/method_definition/parameter_helper.rb
@@ -10,12 +10,24 @@ class Sinclair
     # @see ParameterBuilder
     module ParameterHelper
       class << self
+        # Maps an array of parameters into an Array of +Strings+
+        #
+        # The code takes advantage of ruby splitting parameters and named
+        # parameters (_defaults) from a list of arguments on method call
+        #
+        # @return [Array<String>]
         def parameters_for(*parameters, **_defaults, &block)
           return [] unless parameters
 
           parameters.map(&block)
         end
 
+        # Maps a hash of defaults parameters into an Array of +Strings+
+        #
+        # The code takes advantage of ruby splitting parameters and named
+        # parameters (_defaults) from a list of arguments on method call
+        #
+        # @return [Array<String>]
         def parameteres_defaults_for(*_parameters, **defaults, &block)
           return [] unless defaults
 
