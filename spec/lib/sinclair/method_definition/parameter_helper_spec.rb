71e8ac27f1583c7904cf22670e8074340ed504c5
darthjee <darthjee@gmail.com>
2023-03-15 07:03:00 -0300

    Add simple parameter help spec

--- spec/lib/sinclair/method_definition/parameter_helper_spec.rb
+++ spec/lib/sinclair/method_definition/parameter_helper_spec.rb
@@ -3,4 +3,21 @@
 require 'spec_helper'
 
 describe Sinclair::MethodDefinition::ParameterHelper do
+  describe '.parameters_from' do
+    context 'when parameters are not named' do
+      context 'when there are no defaults' do
+        it 'returns a list of parameters' do
+          expect(described_class.parameters_from(%i[a b]))
+            .to eq(%w[a b])
+        end
+      end
+
+      context 'when there are defaults' do
+        it 'returns a list of parameters' do
+          expect(described_class.parameters_from([{a: 10,  b: 20}]))
+            .to eq(['a = 10', 'b = 20'])
+        end
+      end
+    end
+  end
 end
