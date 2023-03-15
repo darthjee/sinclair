b2e40555394b7ddebb450cf0e1c9213f9e309fe6
darthjee <darthjee@gmail.com>
2023-03-15 07:13:08 -0300

    Add spec over named parameters

--- spec/lib/sinclair/method_definition/parameter_helper_spec.rb
+++ spec/lib/sinclair/method_definition/parameter_helper_spec.rb
@@ -13,9 +13,33 @@ describe Sinclair::MethodDefinition::ParameterHelper do
       end
 
       context 'when there are defaults' do
+        let(:parameters) do
+          [{a: 10,  b: 'word', c: true, d: false}]
+        end
+
         it 'returns a list of parameters' do
-          expect(described_class.parameters_from([{a: 10,  b: 20}]))
-            .to eq(['a = 10', 'b = 20'])
+          expect(described_class.parameters_from(parameters))
+            .to eq(['a = 10', 'b = "word"', 'c = true', 'd = false'])
+        end
+      end
+    end
+
+    context 'when parameters are named' do
+      context 'when there are no defaults' do
+        it 'returns a list of parameters' do
+          expect(described_class.parameters_from(%i[a b], named: true))
+            .to eq(%w[a: b:])
+        end
+      end
+
+      context 'when there are defaults' do
+        let(:parameters) do
+          [{a: 10,  b: 'word', c: true, d: false}]
+        end
+
+        it 'returns a list of parameters' do
+          expect(described_class.parameters_from(parameters, named: true))
+            .to eq(['a: 10', 'b: "word"', 'c: true', 'd: false'])
         end
       end
     end
