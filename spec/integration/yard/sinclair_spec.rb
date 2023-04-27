d2aab9d545687abb6429e36576c3613892447bc1
darthjee <darthjee@gmail.com>
2023-04-27 17:17:16 -0300

    Use better spec format

--- spec/integration/yard/sinclair_spec.rb
+++ spec/integration/yard/sinclair_spec.rb
@@ -4,10 +4,8 @@ require 'spec_helper'
 
 describe Sinclair do
   describe 'yard' do
-    let(:klass)         { Class.new(MyModel) }
-    let(:instance)      { klass.new }
-    let(:builder)       { described_class.new(klass) }
-    let(:default_value) { 10 }
+    let(:klass)   { Class.new(MyModel) }
+    let(:builder) { described_class.new(klass) }
 
     describe 'Using cache' do
       subject(:server) { Server.new }
@@ -85,38 +83,21 @@ describe Sinclair do
       end
     end
 
-    describe '#build' do
-      before do
-        value = default_value
+    describe 'Stand alone usage' do
+      it 'builds the methods' do
+        builder = Sinclair.new(klass)
+
+        value = 10
         builder.add_method(:default_value) { value }
         builder.add_method(:value, '@value || default_value')
         builder.add_method(:value=) { |val| @value = val }
-      end
+        builder.build
 
-      describe 'after the build' do
-        before { builder.build }
+        instance = klass.new
 
-        it 'creates the expected methods' do
-          expect(instance.value).to eq(10)
-        end
-
-        context 'when default value is overwritten' do
-          before do
-            instance.value = 20
-          end
-
-          it 'returns the new written value' do
-            expect(instance.value).to eq(20)
-          end
-        end
-      end
-
-      context 'when calling the build' do
-        it do
-          expect do
-            builder.build
-          end.to change { instance.respond_to?(:default_value) }.to(true)
-        end
+        expect(instance.value).to eq(10)
+        instance.value = 20
+        expect(instance.value).to eq(20)
       end
     end
   end
