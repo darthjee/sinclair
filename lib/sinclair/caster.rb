0eba1e5cd6c827a1a600fd229a6c88a9da999b4f
darthjee <darthjee@gmail.com>
2023-04-28 08:23:10 -0300

    Extract class methods

--- lib/sinclair/caster.rb
+++ lib/sinclair/caster.rb
@@ -12,60 +12,8 @@ class Sinclair
   # Inheritance grants the hability to have different casting for different
   # purposes / applications / gems
   class Caster
-    class << self
-      def cast_with(key, method_name = nil, &block)
-        caster = instance_for(method_name, &block)
-
-        return class_casters[key] = caster if key.is_a?(Class)
-
-        casters[key] = caster
-      end
-
-      def cast(value, key, **opts)
-        caster_for(key).cast(value, **opts)
-      end
-
-      def caster_for(key)
-        return casters[key] if casters.key?(key)
-
-        caster_for_class(key) || superclas_caster_for(key) || new { |value| value }
-      end
-
-      protected
-
-      def superclas_caster_for(key)
-        return if master_class?
-
-        superclass.caster_for(key)
-      end
-
-      def caster_for_class(klass)
-        class_casters.find do |klazz, _|
-          klass == klazz || klass < klazz
-        end&.second
-      end
-
-      def instance_for(method_name, &block)
-        return new(&block) unless method_name
-        return method_name if method_name.is_a?(Caster)
-
-        new(&method_name)
-      end
-
-      private
-
-      def casters
-        @casters ||= {}
-      end
-
-      def class_casters
-        @class_casters ||= {}
-      end
-
-      def master_class?
-        @master_class ||= self == Sinclair::Caster
-      end
-    end
+    autoload :ClassMethods, 'sinclair/caster/class_methods'
+    extend Caster::ClassMethods
 
     def initialize(&block)
       @block = block.to_proc
---
+++ lib/sinclair/caster/class_methods.rb
@@ -0,0 +1,70 @@
+# frozen_string_literal: true
+
+class Sinclair
+  # @api public
+  # @author darhtjee
+  #
+  # Class responsible for defining how to and casting values
+  #
+  # First the class needs to be configured using {.cast_with} and later
+  # a value can be cast by using {.cast} or {.caster_for}
+  #
+  # Inheritance grants the hability to have different casting for different
+  # purposes / applications / gems
+  class Caster
+    module ClassMethods
+      def cast_with(key, method_name = nil, &block)
+        caster = instance_for(method_name, &block)
+
+        return class_casters[key] = caster if key.is_a?(Class)
+
+        casters[key] = caster
+      end
+
+      def cast(value, key, **opts)
+        caster_for(key).cast(value, **opts)
+      end
+
+      def caster_for(key)
+        return casters[key] if casters.key?(key)
+
+        caster_for_class(key) || superclas_caster_for(key) || new { |value| value }
+      end
+
+      protected
+
+      def superclas_caster_for(key)
+        return if master_class?
+
+        superclass.caster_for(key)
+      end
+
+      def caster_for_class(klass)
+        class_casters.find do |klazz, _|
+          klass == klazz || klass < klazz
+        end&.second
+      end
+
+      def instance_for(method_name, &block)
+        return new(&block) unless method_name
+        return method_name if method_name.is_a?(Caster)
+
+        new(&method_name)
+      end
+
+      private
+
+      def casters
+        @casters ||= {}
+      end
+
+      def class_casters
+        @class_casters ||= {}
+      end
+
+      def master_class?
+        @master_class ||= self == Sinclair::Caster
+      end
+    end
+  end
+end
