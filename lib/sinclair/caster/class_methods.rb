35cf7964676f9df006eb8307382e9b804e00b1ba
darthjee <darthjee@gmail.com>
2023-04-28 16:18:19 -0300

    Add documentation on privates methods

--- lib/sinclair/caster/class_methods.rb
+++ lib/sinclair/caster/class_methods.rb
@@ -35,18 +35,50 @@ class Sinclair
 
       protected
 
+      # @api private
+      #
+      # Returns a caster from the superclass
+      #
+      # @param key [Symbol,Class] key to be checked
+      #
+      # @see caster_for
+      # @return [Caster]
       def superclas_caster_for(key)
         return if master_caster?
 
         superclass.caster_for(key)
       end
 
+      # @api private
+      #
+      # Returns a caster searching for using class as key
+      #
+      # This is called by {#caster_for} any time key is a class
+      #
+      # @param klass [Class] class to be used in the search
+      #
+      # When the given class is not registered, a caster for a parent
+      # class is returned
+      #
+      # @return [Caster]
       def caster_for_class(klass)
         class_casters.find do |klazz, _|
           klass <= klazz
         end&.second
       end
 
+      # Returns a new instance {Caster}
+      #
+      # @overload instance_for(method_name, &block)
+      #   @param method_name [Symbol] method to be called in the model
+      #   @param block [Proc] block to perform the casting
+      #
+      #   When +method_name+ is not given, the block is used
+      #
+      # @overload instance_for(caster)
+      #   @param caster [Caster] instance of caster to be returned
+      #
+      # @return [Caster]
       def instance_for(method_name, &block)
         return new(&block) unless method_name
         return method_name if method_name.is_a?(Caster)
@@ -56,14 +88,35 @@ class Sinclair
 
       private
 
+      # @api private
+      # @private
+      #
+      # Caster map stored by +Symbols+
+      #
+      # @return [Hash<Symbol,Caster>]
       def casters
         @casters ||= {}
       end
 
+      # @api private
+      # @private
+      #
+      # Caster map stored by +Classs+
+      #
+      # @return [Hash<Class,Caster>]
       def class_casters
         @class_casters ||= {}
       end
 
+      # @api private
+      # @private
+      #
+      # Chack if the caster class is a master
+      #
+      # A master caster never checks if a superclass has a caster
+      #
+      # @see master_caster
+      # @return [TrueClass,FalseClass]
       def master_caster?
         @master_caster
       end
