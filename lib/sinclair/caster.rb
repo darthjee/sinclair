6a56c2f6947b710a3ed24b9b1f6eedea066638ff
darthjee <darthjee@gmail.com>
2023-04-28 13:44:36 -0300

    Add basic instance documentation

--- lib/sinclair/caster.rb
+++ lib/sinclair/caster.rb
@@ -16,10 +16,20 @@ class Sinclair
     extend Caster::ClassMethods
     master_caster
 
+    # @param block [Proc] Proc to be used when converting the value object
     def initialize(&block)
       @block = block.to_proc
     end
 
+    # Cast a value using the given the set +block+
+    #
+    # @param value [Object] value to be converted
+    # @param opts [Hash] options to be sent to the block
+    #
+    # When the block does not accept options, those
+    # are not passed
+    #
+    # @return [Object] the result of the converting block
     def cast(value, **opts)
       options = opts.select do |key, _|
         options_keys.include?(key)
@@ -30,12 +40,23 @@ class Sinclair
 
     private
 
+    # @private
+    # Keys of options accepted by the block
+    #
+    # @return [Array<Symbol>]
     def options_keys
       @options_keys ||= block.parameters.select do |(type, _)|
         %i[key keyreq].include? type
       end.map(&:second)
     end
 
+    # @method block
+    # @api private
+    # @private
+    #
+    # Proc to be used when converting the value object
+    #
+    # @return [Proc]
     attr_reader :block
 
     cast_with(:string, :to_s)
