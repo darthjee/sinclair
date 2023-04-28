92b182fba0f27cc77bb3e765ada146b368fdc230
darthjee <darthjee@gmail.com>
2023-04-28 12:06:15 -0300

    Do not handle missing options

--- spec/lib/sinclair/caster_spec.rb
+++ spec/lib/sinclair/caster_spec.rb
@@ -8,7 +8,7 @@ describe Sinclair::Caster do
   let(:caster_class) { Class.new(described_class) }
 
   describe '.cast' do
-    context 'when no options are given and the block requires none' do
+    context 'when no options are given and the block accepts none' do
       let(:method_name) { :to_s }
 
       it 'uses the block to transform the value' do
@@ -16,11 +16,47 @@ describe Sinclair::Caster do
       end
     end
 
-    context 'when options are given and the block requires none' do
+    context 'when options are given and the block accepts none' do
       let(:method_name) { :to_i }
 
       it 'uses the block to transform the value' do
         expect(caster.cast('10', extra: true)).to eq(10)
+      end
+    end
+
+    context 'when no options are given and the block accepts options' do
+      subject(:caster) do
+        caster_class.new do |value, sum: 5|
+          (value.to_i + sum).to_s
+        end
+      end
+
+      it 'uses the block to transform the value' do
+        expect(caster.cast('10')).to eq('15')
+      end
+    end
+
+    context 'when options are given and the block accepts options' do
+      subject(:caster) do
+        caster_class.new do |value, sum:|
+          (value.to_i + sum).to_s
+        end
+      end
+
+      it 'uses the block to transform the value' do
+        expect(caster.cast('10', sum: 5)).to eq('15')
+      end
+    end
+
+    context 'when no options are given and the block requires options' do
+      subject(:caster) do
+        caster_class.new do |value, sum:|
+          (value.to_i + sum).to_s
+        end
+      end
+
+      it 'uses the block to transform the value' do
+        expect { caster.cast('10') }.to raise_error(ArgumentError)
       end
     end
   end
