4e7d720fa31cc74d12ecc27a7211f9df153f0a52
darthjee <darthjee@gmail.com>
2023-04-28 10:03:34 -0300

    Splic lass and instance methods spec

--- spec/lib/sinclair/caster_spec.rb
+++ spec/lib/sinclair/caster_spec.rb
@@ -3,159 +3,16 @@
 require 'spec_helper'
 
 describe Sinclair::Caster do
-  subject(:caster) { Class.new(described_class) }
+  subject(:caster) { caster_class.new(&method_name) }
 
-  describe '.cast_with' do
-    let(:value)       { instance_double('value', to_p: final_value) }
-    let(:final_value) { Random.rand(100) }
-
-    context 'when a proc is given' do
-      it do
-        expect { caster.cast_with(:problem, &:to_p) }
-          .not_to raise_error
-      end
-
-      context 'when casting is called' do
-        before { caster.cast_with(:problem, &:to_p) }
-
-        it 'returns the cast value' do
-          expect(caster.cast(value, :problem)).to eq(final_value)
-        end
-      end
-    end
-
-    context 'when a proc with two arguments is given' do
-      it do
-        expect { caster.cast_with(:problem) { |v, **_opts| v.to_p } }
-          .not_to raise_error
-      end
-
-      context 'when casting is called' do
-        before { caster.cast_with(:problem) { |v, sum:| v.to_p + sum } }
-
-        it 'returns the cast value' do
-          expect(caster.cast(value, :problem, sum: 2))
-            .to eq(final_value + 2)
-        end
-      end
-    end
-
-    context 'when a symbol is given' do
-      let(:instance) { described_class.new(&:to_p) }
-
-      it do
-        expect { caster.cast_with(:problem, instance) }
-          .not_to raise_error
-      end
-
-      context 'when casting is called' do
-        before { caster.cast_with(:problem, instance) }
-
-        it 'returns the cast value' do
-          expect(caster.cast(value, :problem)).to eq(final_value)
-        end
-      end
-    end
-
-    context 'when a caster is given is given' do
-      it do
-        expect { caster.cast_with(:problem, :to_p) }
-          .not_to raise_error
-      end
-
-      context 'when casting is called' do
-        before { caster.cast_with(:problem, :to_p) }
-
-        it 'returns the cast value' do
-          expect(caster.cast(value, :problem)).to eq(final_value)
-        end
-      end
-    end
-
-    context 'when key is a class' do
-      it do
-        expect { caster.cast_with(Integer, :to_i) }
-          .not_to raise_error
-      end
-
-      context 'when casting is called' do
-        before { caster.cast_with(Integer, :to_i) }
-
-        it 'returns the cast value' do
-          expect(caster.cast('10', Integer)).to eq(10)
-        end
-      end
-    end
-
-    context 'when key is a superclass' do
-      it do
-        expect { caster.cast_with(Numeric, :to_i) }
-          .not_to raise_error
-      end
-
-      context 'when casting is called with the child class' do
-        before { caster.cast_with(Numeric, :to_i) }
-
-        it 'returns the cast value' do
-          expect(caster.cast('10', Integer)).to eq(10)
-        end
-      end
-    end
-  end
-
-  describe '.caster_for' do
-    context 'when the key has been defined with a symbol key' do
-      before { caster.cast_with(:problem, :to_p) }
-
-      it do
-        expect(caster.caster_for(:problem))
-          .to be_a(described_class)
-      end
-    end
-
-    context 'when the key has not been defined' do
-      it do
-        expect(caster.caster_for(:problem))
-          .to be_a(described_class)
-      end
-    end
-  end
+  let(:caster_class) { Class.new(described_class) }
 
   describe '.cast' do
-    let(:value) { values.sample }
-    let(:values) do
-      [Random.rand, 'some string', { key: 10 }, Object.new, Class.new, [2, 3]]
-    end
+    context 'when no options are given and the block requires none' do
+      let(:method_name) { :to_s }
 
-    context 'when klass is nil' do
-      it 'returns the value' do
-        expect(caster.cast(value, nil))
-          .to eq(value)
-      end
-    end
-
-    context 'when class is :string' do
-      it 'returns the value as string' do
-        expect(caster.cast(value, :string))
-          .to eq(value.to_s)
-      end
-    end
-
-    context 'when class is :integer' do
-      let(:value) { '10.5' }
-
-      it 'returns the value as integer' do
-        expect(caster.cast(value, :integer))
-          .to eq(10)
-      end
-    end
-
-    context 'when class is :float' do
-      let(:value) { '10.5' }
-
-      it 'returns the value as float' do
-        expect(caster.cast(value, :float))
-          .to eq(10.5)
+      it 'uses the block to transform the value' do
+        expect(caster.cast(10)).to eq('10')
       end
     end
   end
