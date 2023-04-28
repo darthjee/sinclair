a3ad093336c642cb48e5565b87dd1bfc7e26294d
darthjee <darthjee@gmail.com>
2023-04-28 13:05:57 -0300

    Add master class spec

--- spec/lib/sinclair/caster/class_methods_spec.rb
+++ spec/lib/sinclair/caster/class_methods_spec.rb
@@ -159,4 +159,12 @@ describe Sinclair::Caster::ClassMethods do
       end
     end
   end
+
+  describe '.master_class' do
+    it 'ignores superclass registered casters' do
+      expect { caster.master_class }
+        .to change { caster.cast('10', :integer) }
+        .from(10).to('10')
+    end
+  end
 end
