07e8fd994cef67466864bf81f9182a770e0bbdbb
darthjee <darthjee@gmail.com>
2023-04-28 20:40:43 -0300

    Reoganize config

--- lib/sinclair/method_definition/stringifier.rb
+++ lib/sinclair/method_definition/stringifier.rb
@@ -13,10 +13,8 @@ class Sinclair
 
       cast_with(NilClass) { 'nil' }
       cast_with(Symbol) { |value| ":#{value}" }
-      cast_with(Class, :to_s)
-      cast_with(Hash, :to_s)
-      cast_with(Array, :to_s)
-      cast_with(Object, :to_json)
+      cast_with(String, :to_json)
+      cast_with(Object, :to_s)
 
       # Convert a value to a string format
       #
--- spec/support/models/ruby_string_caster.rb
+++ spec/support/models/ruby_string_caster.rb
@@ -3,10 +3,8 @@ class RubyStringCaster < Sinclair::Caster
 
   cast_with(NilClass) { 'nil' }
   cast_with(Symbol) { |value| ":#{value}" }
-  cast_with(Class, :to_s)
-  cast_with(Hash, :to_s)
-  cast_with(Array, :to_s)
-  cast_with(Object, :to_json)
+  cast_with(String, :to_json)
+  cast_with(Object, :to_s)
 
   def self.to_ruby_string(value)
     cast(value, value.class)
