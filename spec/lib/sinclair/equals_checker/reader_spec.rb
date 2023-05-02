486b2dd5d7325acf867aabc76f5a6e183718c030
darthjee <darthjee@gmail.com>
2023-05-02 10:51:49 -0300

    Add spec over class method

--- spec/lib/sinclair/equals_checker/reader_spec.rb
+++ spec/lib/sinclair/equals_checker/reader_spec.rb
@@ -6,13 +6,36 @@ describe Sinclair::EqualsChecker::Reader do
   subject(:reader) { described_class.new(attribute) }
 
   let(:attribute) { :information }
-  let(:model) { SampleModel.new(name: name, age: age) }
-  let(:name)  { "The Name" }
-  let(:age)   { 25 }
+  let(:model)     { SampleModel.new(name: name, age: age) }
+  let(:name)      { 'The Name' }
+  let(:age)       { 25 }
+
+  describe '.attributes_match?' do
+    let(:other)      { SampleModel.new(name: other_name) }
+    let(:attribute)  { :name }
+
+    context 'when the value match' do
+      let(:other_name) { name }
+
+      it do
+        expect(described_class)
+          .to be_attributes_match(attribute, model, other)
+      end
+    end
+
+    context 'when the value does not match' do
+      let(:other_name) { 'Other Name' }
+
+      it do
+        expect(described_class)
+          .to be_attributes_match(attribute, model, other)
+      end
+    end
+  end
 
   describe '#read_from' do
     context 'when reading from a method' do
-      it "returns the value from the method" do
+      it 'returns the value from the method' do
         expect(reader.read_from(model)).to eq('The Name: 25 yo')
       end
     end
@@ -24,7 +47,7 @@ describe Sinclair::EqualsChecker::Reader do
         model.instance_variable_set(:@inner_variable, 301)
       end
 
-      it "returns the value from the variable" do
+      it 'returns the value from the variable' do
         expect(reader.read_from(model)).to eq(301)
       end
     end
