596db87d049d186025fb4505c5d4f8afbc046bd0
darthjee <darthjee@gmail.com>
2023-05-02 10:31:12 -0300

    Update doc

--- lib/sinclair/equals_checker/reader.rb
+++ lib/sinclair/equals_checker/reader.rb
@@ -7,39 +7,51 @@ class Sinclair
     #
     # Class capable of reading an attribute from models
     class Reader
-      class << self
-        # @private
-        # @api private
-        #
-        # Checks if two attributes from 2 object match
-        #
-        # @param attribute [Symbol] attribute name
-        # @param model [Object] object to be compared with other
-        # @param other [Object] object to be compared with model
-        #
-        # @see #match?
-        # @return [TrueClass,FalseClass]
-        def attributes_match?(attribute, model, other)
-          reader = new(attribute)
+      # @private
+      # @api private
+      #
+      # Checks if two attributes from 2 object match
+      #
+      # @param attribute [Symbol] attribute name
+      # @param model [Object] object to be compared with other
+      # @param other [Object] object to be compared with model
+      #
+      # @see #match?
+      # @return [TrueClass,FalseClass]
+      def self.attributes_match?(attribute, model, other)
+        reader = new(attribute)
 
-          reader.read_from(model) == reader.read_from(other)
-        end
+        reader.read_from(model) == reader.read_from(other)
       end
 
+      # @param attribute [Symbol] name of the attribute (method or variable)
+      #   to be accessed in the models
       def initialize(attribute)
         @attribute = attribute
       end
 
+      # Reads the +attribute+ from the model
+      #
+      # When attribute is a method name, calls that method on the model
+      # 
+      # When attribute is an instance variable name, that is read directly from the model
+      #
+      # @param model [Object] the model to be read
+      #
+      # @return [Object]
       def read_from(model)
-        if attribute.to_s.match?(/^@.*/)
-          model.instance_variable_get(attribute)
-        else
-          model.send(attribute)
-        end
+        return model.send(attribute) unless attribute.to_s.match?(/^@.*/)
+
+        model.instance_variable_get(attribute)
       end
 
       private
 
+      # @attr_reader :attribute
+      #
+      # Reads the attribute that will be used to extract the value
+      #
+      # @return [Symbol]
       attr_reader :attribute
     end
   end
