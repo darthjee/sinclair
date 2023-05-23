5de47857b4df65c96345081dd13d66b30335e744
Fernando Favini <darthjee@gmail.com>
2023-05-23 23:21:57 +0200

    Build fix (#94)

--- spec/lib/sinclair/class_methods_spec.rb
+++ spec/lib/sinclair/class_methods_spec.rb
@@ -10,7 +10,7 @@ describe Sinclair::ClassMethods do
   let(:dummy_class)   { Class.new }
   let(:builder_class) { Sinclair }
 
-  describe '#build' do
+  describe '.build' do
     let(:block) do
       method_name = :some_method
       value = 1
@@ -21,13 +21,13 @@ describe Sinclair::ClassMethods do
     end
 
     it 'executes the block and builds' do
-      expect { builder_class.build(dummy_class, options, &block) }
+      expect { builder_class.build(dummy_class, **options, &block) }
         .to add_method(:some_method).to(dummy_class)
     end
 
     context 'when the method is built and called' do
       before do
-        builder_class.build(dummy_class, options, &block)
+        builder_class.build(dummy_class, **options, &block)
       end
 
       it 'returns the value' do
@@ -46,8 +46,28 @@ describe Sinclair::ClassMethods do
       end
 
       it 'executes the block and builds' do
-        expect { builder_class.build(dummy_class, options) }
+        expect { builder_class.build(dummy_class, **options) }
           .to add_method(:some_method).to(dummy_class)
+      end
+    end
+
+    context 'when a block is given and initialization contains more arguments' do
+      let(:builder_class) { ComplexBuilder }
+      let(:value)         { Random.rand(10..30) }
+      let(:power)         { Random.rand(3..5) }
+      let(:result)        { value**power }
+
+      it 'executes the block and builds' do
+        expect { builder_class.build(dummy_class, value, power, &:add_default) }
+          .to add_method(:result).to(dummy_class)
+      end
+
+      context 'when the method is called' do
+        before { builder_class.build(dummy_class, value, power, &:add_default) }
+
+        it 'returns the evaluated value' do
+          expect(instance.result).to eq(result)
+        end
       end
     end
   end
