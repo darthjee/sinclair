0619d638e3a2024706f53ad2abe3a944f22657ab
darthjee <darthjee@gmail.com>
2023-07-25 12:49:28 -0300

    Remove wrong doc

--- lib/sinclair/settable.rb
+++ lib/sinclair/settable.rb
@@ -1,30 +1,6 @@
 # frozen_string_literal: true
 
 class Sinclair
-  # @api public
-  # @author darthjee
-  #
-  # Module to be extended allowing configurations from environment
-  #
-  # @example
-  #   class MyAppClient
-  #     extend Sinclair::Settable
-  #
-  #     settings_prefix 'MY_APP'
-  #
-  #     with_settings :username, :password, host: 'my-host.com'
-  #   end
-  #
-  #   ENV['MY_APP_USERNAME'] = 'my_login'
-  #
-  #   MyAppClient.username # returns 'my_login'
-  #   MyAppClient.password # returns nil
-  #   MyAppClient.host     # returns 'my-host.com'
-  #
-  #   ENV['MY_APP_HOST'] = 'other-host.com'
-  #
-  #   MyAppClient.host     # returns 'other-host.com'
-  #
   module Settable
     autoload :Builder, 'sinclair/settable/builder'
 
