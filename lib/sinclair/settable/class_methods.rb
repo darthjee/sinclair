a574b8c76e9233aac7723305e6f4cffb10ef3353
darthjee <darthjee@gmail.com>
2023-07-26 17:45:47 -0300

    Add class method documentation

--- lib/sinclair/settable/class_methods.rb
+++ lib/sinclair/settable/class_methods.rb
@@ -2,7 +2,22 @@
 
 class Sinclair
   module Settable
+    # @api public
+    # @author darthjee
+    #
+    # Class methods availabe inside a Settable module
+    #
+    # The methods should help configuring the settable
+    #
+    # @see #read_with
     module ClassMethods
+      # Register and return a block for reading a setting
+      #
+      # @param read_block [Proc] the block that will be used when
+      #   reading a setting key
+      #
+      # When the block is called, it will receive the key and any
+      # given options
       def read_with(&read_block)
         return @read_block = read_block if read_block
         return @read_block if @read_block
@@ -10,6 +25,11 @@ class Sinclair
         superclass_settable&.read_with
       end
 
+      private
+
+      # Returns a {Settable} module representing a superclass
+      #
+      # @return [Module]
       def superclass_settable
         included_modules.find do |modu|
           modu <= Sinclair::Settable
