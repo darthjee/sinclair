df50cd15898b2be903c6278837447632238d9cfd
darthjee <darthjee@gmail.com>
2023-07-28 14:10:28 -0300

    Add spec

--- spec/lib/sinclair/chain_settable_spec.rb
+++ spec/lib/sinclair/chain_settable_spec.rb
@@ -8,6 +8,7 @@ describe Sinclair::ChainSettable do
       extend Sinclair::ChainSettable
 
       source :app_client, Class.new(AppClient)
+      source :app_client, Class.new(MyAppClient)
 
       with_settings :username, :password, :host, :port
     end
@@ -16,10 +17,21 @@ describe Sinclair::ChainSettable do
   let(:username) { 'my_login' }
   let(:password) { Random.rand(10_000).to_s }
 
-  let(:username_key) { 'USERNAME' }
-  let(:password_key) { 'PASSWORD' }
-  let(:host_key)     { 'HOST' }
-  let(:port_key)     { 'PORT' }
+  context 'when the first setting finds the data' do
+    let(:username_key) { 'USERNAME' }
+    let(:password_key) { 'PASSWORD' }
+    let(:host_key)     { 'HOST' }
+    let(:port_key)     { 'PORT' }
 
-  it_behaves_like 'settings reading'
+    it_behaves_like 'settings reading'
+  end
+
+  context 'when the second setting finds the data' do
+    let(:username_key) { 'MY_APP_USERNAME' }
+    let(:password_key) { 'MY_APP_PASSWORD' }
+    let(:host_key)     { 'MY_APP_HOST' }
+    let(:port_key)     { 'MY_APP_PORT' }
+
+    it_behaves_like 'settings reading'
+  end
 end
